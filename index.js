3f07eda1389de1a15ff3bcf84ead88d54fd17465
Jose Kupeka Muhlanga <jose.muhlanga@student.moringaschool.com>
2022-08-20 22:29:51 +0300

    Completed on Aug 20

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    let result = customerName.toUpperCase()
+    return customerName=result;
+}
+
+var bestCustomer;
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';    
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'bob guy';
+
+function changeLeastFavoriteCustomer(){ 
+    leastFavoriteCustomer = 'Doom Guy';
+}
+
+
+
+
